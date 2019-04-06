9638051bfb5844a7c1300df37d031d1b7646d8c4
Karolina Kawalec <kkawalec.ext-madiff@ogury.co>
2019-04-07 00:39:21 +0200

    Handle header scrolling styling

--- src/js/layouts/PageHeader.tsx
+++ src/js/layouts/PageHeader.tsx
@@ -2,21 +2,45 @@ import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import { withStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import ChangeLangMenu from 'layouts/components/ChangeLangMenu';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Translate } from 'react-redux-i18n';
 
+import ChangeLangMenu from 'layouts/components/ChangeLangMenu';
+
+// TODO: Add proper image
+const headerImg = require('../../images/header-image.jpg');
+
 const styles = {
-  root: {
+  toolBar: {
     flexGrow: 1,
     justifyContent: 'center',
   },
+  appBar: {
+    marginBottom: 16,
+  },
+  headerImg: {
+    height: 500,
+    width: 'auto',
+    position: 'absolute' as any,
+    top: 0,
+  },
+};
+
+const transparentStyle = {
+  boxShadow: '0 0 0 0 rgb(0, 0, 0, 0)',
+  backgroundColor: 'rgb(0, 0, 0, 0)',
 };
 
 interface IPageHeaderProps {
   classes: {
-    root: string,
+    appBar: string,
+    toolBar: string,
+    headerImg: string,
   };
+}
+
+interface IPageHeaderState {
+  isScrolled: boolean;
 }
 
 const links = [
@@ -26,7 +50,28 @@ const links = [
   { to: '#contact', label: <Translate value="contact.label" />, icon: null },
 ];
 
-class PageHeader extends React.Component<IPageHeaderProps, {}> {
+class PageHeader extends React.Component<IPageHeaderProps, IPageHeaderState> {
+  public state = {
+    isScrolled: false,
+  };
+
+  public componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  public componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  public handleScroll = () => {
+    const scrollTop = window.pageYOffset ||
+      ((document.documentElement || document.body.parentNode || document.body) as any).scrollTop;
+    const isScrolled = scrollTop !== 0;
+
+    if (this.state.isScrolled !== isScrolled) {
+      this.setState({ isScrolled });
+    }
+  }
 
   public renderButtonLink(): JSX.Element[] {
     return links.map(link => (
@@ -40,13 +85,23 @@ class PageHeader extends React.Component<IPageHeaderProps, {}> {
 
   public render(): JSX.Element {
     const { classes } = this.props;
+    const { isScrolled } = this.state;
+    const style = isScrolled ? {} : transparentStyle;
+
     return (
-      <AppBar position="sticky" color="primary" style={{ marginBottom: 16 }} >
-        <Toolbar className={classes.root}>
+      <Fragment>
+      <AppBar className={classes.appBar} position="sticky" color="primary" style={style}>
+        <Toolbar className={classes.toolBar} style={style}>
           {this.renderButtonLink()}
           <ChangeLangMenu />
         </Toolbar>
       </AppBar>
+      <img
+        src={headerImg}
+        className={classes.headerImg}
+        alt="Crystal Tyranids - Miniature Painting Studio"
+      />
+      </Fragment>
     );
   }
 }
